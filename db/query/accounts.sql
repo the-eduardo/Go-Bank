ae388824fc935aabad35059741d6ab60526fdede
eduardo <eduardo.xbox@live.com>
2022-07-06 23:42:45 -0300

    SqlDialectMappings File

--- db/query/accounts.sql
+++ db/query/accounts.sql
@@ -9,4 +9,13 @@ INSERT INTO accounts (
          )
     RETURNING *;
 
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
 

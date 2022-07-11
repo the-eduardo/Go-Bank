ba562de26400517009dde8e258c9f1106f00210c
eduardo <eduardo.xbox@live.com>
2022-07-11 02:56:03 -0300

    Added queries order to avoid deadlock in DB transactions

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -90,18 +90,34 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 		// Get the updated accounts balance
-
-		result.FromAccount, err = queries.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-
-		result.ToAccount, err = queries.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, queries, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, queries, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
 		return nil
 	})
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	queries *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = queries.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+	account2, err = queries.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
 }
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -103,3 +103,50 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance, "Final balance is not equal")
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance, "Final balance is not equal")
 }
+
+// CODE COPY for check DeadLock
+
+func TestTransferTxDeadLock(t *testing.T) {
+	store := NewStore(testDB)
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+	// run n concurrent transfer transactions
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		//txName := fmt.Sprintf("tx %d", i+1)
+		go func() {
+			//ctx := context.WithValue(context.Background(), txKey, txName)
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+			errs <- err
+		}()
+	}
+	//check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err, "TransferTx")
+	}
+	// check final balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err, "GetAccount")
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err, "GetAccount")
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance, "Final balance is not equal")
+	require.Equal(t, account2.Balance, updatedAccount2.Balance, "Final balance is not equal")
+}

4261541cd66f4738bbdd3bc2c6691284cb5792ae
eduardo <eduardo.xbox@live.com>
2022-08-12 02:55:39 -0300

    [feat] viper: load configurations from file with environment variables.

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"github.com/the-eduardo/Go-Bank/util"
 	"log"
 	"os"
 	"testing"
@@ -9,18 +10,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Error loading config: ", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db ", err)
 	}

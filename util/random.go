141f11ee9e050bcd4b0ca547ec84d93429ed6306
Eduardo <eduardo.xbox@live.com>
2024-02-23 23:46:45 -0300

    changed math/rand to crypto/rand as it should be better

--- util/random.go
+++ util/random.go
@@ -1,7 +1,8 @@
 package util
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math/big"
 	"strings"
 )
 
@@ -9,16 +10,27 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 // RandomInt Generate a random integer between min and max
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max-min+1)
+	diff := max - min
+	nBig, err := rand.Int(rand.Reader, big.NewInt(diff+1))
+	if err != nil {
+		panic(err)
+	}
+	n := nBig.Int64()
+	return min + n
 }
 
 // RandomString Generate a random string of length n
 func RandomString(n int) string {
 	var sb strings.Builder
-	k := len(alphabet)
+	k := big.NewInt(int64(len(alphabet)))
 
 	for i := 0; i < n; i++ {
-		c := alphabet[rand.Intn(k)]
+		indexBig, err := rand.Int(rand.Reader, k)
+		if err != nil {
+			panic(err)
+		}
+		index := indexBig.Int64()
+		c := alphabet[index]
 		sb.WriteByte(c)
 	}
 	return sb.String()
@@ -34,9 +46,15 @@ func RandomMoney() int64 {
 
 func RandomCurrency() string {
 	currencies := []string{USD, EUR, BRL}
-	n := len(currencies)
-	return currencies[rand.Intn(n)]
+	n := big.NewInt(int64(len(currencies)))
+	indexBig, err := rand.Int(rand.Reader, n)
+	if err != nil {
+		panic(err)
+	}
+	index := indexBig.Int64()
+	return currencies[index]
 }
+
 func RandomEmail() string {
 	return RandomString(6) + "@email.com"
 }

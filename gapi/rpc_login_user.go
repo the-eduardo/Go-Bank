34d57619baf9a3a1710ad43428746a42649cb56e
Eduardo <eduardo.xbox@live.com>
2024-07-19 22:07:46 -0300

    Added Violation - Validator in grpc api

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -8,13 +8,18 @@ import (
 	db "github.com/the-eduardo/Go-Bank/db/sqlc"
 	"github.com/the-eduardo/Go-Bank/pb"
 	"github.com/the-eduardo/Go-Bank/util"
+	"github.com/the-eduardo/Go-Bank/val"
 	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	if violations := validateLoginUserRequest(req); violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
@@ -71,4 +76,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return resp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+	return violations
 }
---
+++ val/validator.go
@@ -0,0 +1,53 @@
+package val
+
+import (
+	"fmt"
+	"net/mail"
+	"regexp"
+)
+
+var isValidUsername = regexp.MustCompile(`^[a-zA-Z0-9_]+$`).MatchString
+var isValidFullName = regexp.MustCompile(`^[a-zA-Z\\s]+$`).MatchString
+
+func ValidateString(value string, minLenght int, maxLength int) error {
+	if len(value) < minLenght || len(value) > maxLength {
+		return fmt.Errorf("value length must be between %d and %d characters", minLenght, maxLength)
+	}
+	return nil
+}
+
+func ValidateUsername(value string) error {
+	if err := ValidateString(value, 3, 30); err != nil {
+		return err
+	}
+	if !isValidUsername(value) {
+		return fmt.Errorf("username can only contain letters, numbers and underscores")
+	}
+	return nil
+}
+func ValidateFullName(value string) error {
+	if err := ValidateString(value, 3, 90); err != nil {
+		return err
+	}
+	if !isValidFullName(value) {
+		return fmt.Errorf("names can only contain letters and spaces")
+	}
+	return nil
+}
+
+func ValidatePassword(value string) error {
+	if err := ValidateString(value, 6, 72); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ValidateEmail(value string) error {
+	if err := ValidateString(value, 3, 320); err != nil {
+		return err
+	}
+	if _, err := mail.ParseAddress(value); err != nil {
+		return fmt.Errorf("invalid email address")
+	}
+	return nil
+}
